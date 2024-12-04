21061df93b2b343373062c2ae0b6f467a12ae2ba
Beatriztesser <beatrizeduardabt@gmail>
2024-12-04 08:25:57 -0400

    doidera

---
+++ front-end2/projeto/src/app/listas/page.js
@@ -0,0 +1,62 @@
+export default function Listas(){
+    const racas= ['Vira-lata', 'Pit-bull', 'Pug', 'shitzu', 'Labrador', 'Golden']
+    const racasObj= [
+        {
+            id:1,
+            raca:'Vira-lata',
+
+        },
+
+        {
+            id:2,
+            raca:'Pit-bulllll',
+
+        },
+        {
+            id:3,
+            raca:'Pug',
+
+        },
+        {
+            id:4,
+            raca:'Shitzu',
+
+        },
+        {
+            id:5,
+            raca:'Labrador',
+
+        },
+        {
+            id: 6,
+            raca:'Golden',
+
+        }
+    ]
+    return(
+        <div>
+            <h1>Listas</h1>
+            <p>{racasObj[0].raca}</p>
+            <h2>Lista comum </h2>
+            <ul>
+                {
+                    racas.map((raca,i)=>(
+                        <li key= {i}>{i+1} - {raca}</li>
+                    ))
+                }
+            </ul>
+
+            <div>
+                <h2>Lista de Objetos</h2>
+                {racasObj.map((racaObj)=>(
+                    <div key={racaObj.id}>
+                        <h3>{racaObj.id}</h3>
+                        <p>{racaObj.raca}</p>
+                    </div>   
+                ))}
+            </div>
+            
+            
+        </div>
+    )
+}
--- front-end2/projeto/src/components/header/index.js
+++ front-end2/projeto/src/components/header/index.js
@@ -1,6 +1,7 @@
 'use client';
 import Link from "next/link"
 import {useState} from "react";
+import styles from './header.module.css'
 
 export default function Header () {
     const [showMenu, setShowMenu]=useState(true)
@@ -9,13 +10,14 @@ export default function Header () {
             <button onClick={()=>setShowMenu (!showMenu)}> clique </button>
             {
                 showMenu&&
-                <nav className={styles.nav}>  
-                <ul>
-                    <li> <Link href='/'> Home </Link> </li>
-                    <li> <Link href='/sobre'>Sobre </Link> </li>
-                    <li> <Link href='/contato'> Contato </Link></li>
-                </ul> 
-            </nav>
+                <nav className= {styles.nav}>  
+                    <ul>
+                        <li> <Link href='/'> Home </Link> </li>
+                        <li> <Link href='/sobre'>Sobre </Link> </li>
+                        <li> <Link href='/contato'> Contato </Link></li>
+                        <li> <Link href='/listas'> Listas</Link></li>
+                    </ul> 
+                </nav>
             }
             
         </header>
