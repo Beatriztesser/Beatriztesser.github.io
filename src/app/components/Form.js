9d1c08d666a307591307818f00ce9bce86c47845
Beatriztesser <beatrizeduardabt@gmail.com>
2024-12-19 17:39:43 -0400

    doidera

---
+++ src/app/components/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Form.module.css"
+
+const Form =()=>{
+    return(
+        <div className={styles.formContainer}>
+
+        </div>
+    );
+}
+
+export default Form;
