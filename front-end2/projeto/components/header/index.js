9511ecb2da28294fc043d2c53c1410dadc34d61f
Beatriztesser <beatrizeduardabt@gmail.com>
2025-02-03 20:41:39 -0400

    afff

--- front-end2/projeto/components/header/index.js
+++ front-end2/projeto/components/header/index.js
@@ -6,15 +6,17 @@ import Link from "next/link"
 
 export default function Header(){
     return(
-        <header className={styles.navegador}>
-            <nav className={styles.nav}>
-                <ul>
-                    <li> <Link className={styles.linkMenu} >Médico</Link>
+        <header>
+            <nav>
+                <h1>Algo</h1>
+                <ul className={styles.containerMenu}>
+                    <li> Médico</li>
+                    <li> Médico</li>
+                    <li> Médico</li>
                         <ul className={styles.containerSubMenu}>
-                            <li> <Link className={styles.linkSubMenu} href= "/listar_resgistros" >Listar registros</Link></li>
+                            {/* <li> <Link className={styles.linkSubMenu} href= "/listar_resgistros" >Listar registros</Link></li> */}
                             <li> <Link className={styles.linkSubMenu} href="/criar_novo">Buscar</Link></li>
                         </ul>
-                    </li>
                 </ul>
             </nav>
         </header>
