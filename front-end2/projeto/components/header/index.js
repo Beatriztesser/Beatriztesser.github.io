ea74bc501332ac159148ab964a0c7cd8f3e73506
Beatriztesser <beatrizeduardabt@gmail.com>
2025-02-05 18:46:22 -0400

    casa

--- front-end2/projeto/components/header/index.js
+++ front-end2/projeto/components/header/index.js
@@ -1,25 +1,58 @@
+// 'use client'
+// // import {useState} from React;
+// import styles from './header.module.css'
+// import Link from "next/link"
+// import Image from 'next/image'
+
+
+// export default function Header(){
+//     return(
+//         <header>
+//             <nav>
+//                 <h1>Algo</h1>
+//                 <ul className={styles.containerMenu}>
+//                     <Image src= "/logo.png" alt= "Logo" className="logo" width= "200" height= "200"/>
+//                     <li className={styles.linkMenu}> Médico</li>
+//                     <li className={styles.linkMenu}> Médico</li>
+//                     <li className={styles.linkMenu}> Médico</li>
+//                         <ul className={styles.containerSubMenu}>
+//                             {/* <li> <Link className={styles.linkSubMenu} href= "/listar_resgistros" >Listar registros</Link></li> */}
+//                             <li> <Link className={styles.linkSubMenu} href="/criar_novo">Buscar</Link></li>
+//                         </ul>
+//                 </ul>
+//             </nav>
+//         </header>
+//     )
+// }
+
 'use client'
-// import {useState} from React;
-import styles from './header.module.css'
-import Link from "next/link"
-import Image from 'next/image'
 
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './header.module.css';
 
-export default function Header(){
-    return(
+export default function Header() {
+    return (
         <header>
-            <nav>
-                <h1>Algo</h1>
+            <nav >
+
                 <ul className={styles.containerMenu}>
-                    <li> Médico</li>
-                    <li> Médico</li>
-                    <li> Médico</li>
+                <Image src= "/logo.png" alt= "Logo" className="logo" width= "200" height= "200"/>
+                    <li><Link className={styles.linkMenu} href="#"></Link></li>
+                    <li><Link className={styles.linkMenu} href="#">Link Menu 2</Link></li>
+                    <li><Link className={styles.linkMenu} href="#">Link Menu 3</Link></li>
+                    <li><Link className={styles.linkMenu} href="#">Link Menu 4</Link></li>
+                    <li className={styles.dropdown}>
+                        <Link className={styles.linkMenu} href="#">Link Menu 5</Link>
                         <ul className={styles.containerSubMenu}>
-                            {/* <li> <Link className={styles.linkSubMenu} href= "/listar_resgistros" >Listar registros</Link></li> */}
-                            <li> <Link className={styles.linkSubMenu} href="/criar_novo">Buscar</Link></li>
+                            <li><Link className={styles.linkSubMenu} href="#">Link Sub Menu 1</Link></li>
+                            <li><Link className={styles.linkSubMenu} href="#">Link Sub Menu 2</Link></li>
+                            <li><Link className={styles.linkSubMenu} href="#">Link Sub Menu 3</Link></li>
                         </ul>
+                    </li>
                 </ul>
             </nav>
         </header>
-    )
+    );
 }
+
