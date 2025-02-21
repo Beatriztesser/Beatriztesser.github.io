c14a20b56ee77754bdc86c53b27e9ff5f89a4dcd
Beatriztesser <beatrizeduardabt@gmail.com>
2025-02-21 17:01:31 -0400

    ccc

--- clinica/src/components/header/index.js
+++ clinica/src/components/header/index.js
@@ -6,20 +6,17 @@ import { useState } from 'react';
 import styles from './header.module.css';
 
 export default function Header() {
-    const [menuOpen, setMenuOpen] = useState(false);
-    const [submenuAgendamentoOpen, setSubmenuAgendamentoOpen] = useState(false);
     const [submenuMedicosOpen, setSubmenuMedicosOpen] = useState(false);
-    const [submenuPacienteOpen, setSubmenuPacienteOpen]=useState(false);
+    const [submenuAgendamentoOpen, setSubmenuAgendamentoOpen] = useState(false);
+    const [submenuPacienteOpen, setSubmenuPacienteOpen] = useState(false);
 
     return (
         <header className={styles.header}>
             <div className={styles.divLogo}>
-                        <Image src="/logo.semfundo.png" alt="Logo" className="logo" width="130" height="60" />
-                    </div>
+                <Image src="/logo.semfundo.png" alt="Logo" className="logo" width="130" height="60" />
+            </div>
             <nav className={styles.menuBar}>
-
-                <ul className={`${styles.containerMenu} ${menuOpen ? styles.menuOpen : ''}`}>
-                    
+                <ul className={styles.containerMenu}>
                     <li><Link href="/">Home</Link></li>
                     <li 
                         className={styles.dropdown} 
@@ -32,7 +29,6 @@ export default function Header() {
                                 <li><Link href="/medicos/listar_medicos">Listar médicos</Link></li>
                                 <li><Link href="#">Cadastrar médicos</Link></li>
                                 <li><Link href="#">Editar médicos</Link></li>
-
                             </ul>
                         )}
                     </li>
@@ -44,10 +40,9 @@ export default function Header() {
                         <a href="#">Pacientes</a>
                         {submenuAgendamentoOpen && (
                             <ul className={styles.dropdownMenu}>
-                                <li><Link href= "/pacientes/listar_pacientes">Listar consultas</Link></li>
+                                <li><Link href="/pacientes/listar_pacientes">Listar consultas</Link></li>
                                 <li><Link href="#">Agendar consultas</Link></li>
                                 <li><Link href="#">Editar consultas</Link></li>
-                                
                             </ul>
                         )}
                     </li>
@@ -67,9 +62,6 @@ export default function Header() {
                     </li>
                 </ul>
             </nav>
-
-            
         </header>
     );
 }
-
